e081c67f9ee289cf7f053b8e1aadd0e4162dc0fa
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-10-23 08:38:29 +0100

    Update Product.jsx

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,25 +1,30 @@
-import React from 'react'
-import Link from 'next/link'
-import { urlFor } from '../lib/client'
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { urlFor } from '../lib/client';
 
-const Product = ({ product:{ image, name, slug, price} }) => {
-    function numberWithCommas(x) {
-        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    }
+const Product = ({ product: { image, name, slug, price } }) => {
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
   return (
     <Link href={`/product/${slug.current}`}>
-        <div className="product-card">
-            <img src={urlFor(image && image[0])} alt=""
+      <div className="product-card">
+        {image && image[0] && (
+          <Image
+            src={urlFor(image[0])}
+            alt={name || "Product image"}
             width={250}
             height={250}
-            className="product-image" />
-            <p className="product-name">
-                {name}
-            </p>
-            <p className="product-price">N{numberWithCommas(price)}</p>
-        </div>
+            className="product-image"
+          />
+        )}
+        <p className="product-name">{name}</p>
+        <p className="product-price">N{numberWithCommas(price)}</p>
+      </div>
     </Link>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
