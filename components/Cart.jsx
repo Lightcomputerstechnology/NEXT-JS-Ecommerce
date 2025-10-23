1c8cc84b8899927abb39ef05669d4638fa9b340c
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-10-23 08:16:09 +0100

    Update Cart.jsx

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai';
 import { TiDeleteOutline } from 'react-icons/ti';
 import toast from 'react-hot-toast';
@@ -7,19 +8,18 @@ import toast from 'react-hot-toast';
 import { useStateContext } from '../context/StateContext';
 import { urlFor } from '../lib/client';
 
-
 const Cart = () => {
   const cartRef = useRef();
   const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuanitity, onRemove } = useStateContext();
-
 
   return (
     <div className="cart-wrapper" ref={cartRef}>
       <div className="cart-container">
         <button
-        type="button"
-        className="cart-heading"
-        onClick={() => setShowCart(false)}>
+          type="button"
+          className="cart-heading"
+          onClick={() => setShowCart(false)}
+        >
           <AiOutlineLeft />
           <span className="heading">Your Cart</span>
           <span className="cart-num-items">({totalQuantities} items)</span>
@@ -44,7 +44,13 @@ const Cart = () => {
         <div className="product-container">
           {cartItems.length >= 1 && cartItems.map((item) => (
             <div className="product" key={item._id}>
-              <img src={urlFor(item?.image[0])} className="cart-product-image" />
+              <Image
+                src={urlFor(item?.image[0])}
+                alt={item.name || 'Product Image'}
+                className="cart-product-image"
+                width={150}
+                height={150}
+              />
               <div className="item-desc">
                 <div className="flex top">
                   <h5>{item.name}</h5>
@@ -52,13 +58,15 @@ const Cart = () => {
                 </div>
                 <div className="flex bottom">
                   <div>
-                  <p className="quantity-desc">
-                    <span className="minus" onClick={() => toggleCartItemQuanitity(item._id, 'dec') }>
-                    <AiOutlineMinus />
-                    </span>
-                    <span className="num" onClick="">{item.quantity}</span>
-                    <span className="plus" onClick={() => toggleCartItemQuanitity(item._id, 'inc') }><AiOutlinePlus /></span>
-                  </p>
+                    <p className="quantity-desc">
+                      <span className="minus" onClick={() => toggleCartItemQuanitity(item._id, 'dec')}>
+                        <AiOutlineMinus />
+                      </span>
+                      <span className="num">{item.quantity}</span>
+                      <span className="plus" onClick={() => toggleCartItemQuanitity(item._id, 'inc')}>
+                        <AiOutlinePlus />
+                      </span>
+                    </p>
                   </div>
                   <button
                     type="button"
@@ -72,6 +80,7 @@ const Cart = () => {
             </div>
           ))}
         </div>
+
         {cartItems.length >= 1 && (
           <div className="cart-bottom">
             <div className="total">
@@ -79,12 +88,11 @@ const Cart = () => {
               <h3>N{totalPrice}</h3>
             </div>
             <div className="btn-container">
-              <button type="button" className="btn" onClick={() => setShowCart(false)} >
-                <Link href='/Checkout'>
-                Proceed to Checkout
-                </Link>
-                
-              </button>
+              <Link href='/Checkout' passHref>
+                <button type="button" className="btn" onClick={() => setShowCart(false)}>
+                  Proceed to Checkout
+                </button>
+              </Link>
             </div>
           </div>
         )}
@@ -93,4 +101,4 @@ const Cart = () => {
   )
 }
 
-export default Cart
+export default Cart;
