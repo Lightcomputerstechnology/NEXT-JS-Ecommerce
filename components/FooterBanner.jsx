3fd191295bffa8cbf13480e4570fcb3f3c796227
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-10-23 08:17:24 +0100

    Update FooterBanner.jsx

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -1,30 +1,35 @@
-import React from 'react'
+import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import { urlFor } from '../lib/client';
 import HeadphonePic from '../public/headphones_a_4.webp';
 
 const FooterBanner = ({ FooterBanner }) => {
   return (
     <div className="footer-banner-container">
-        <div className="banner-desc">
-            <div className="">
-                <p>{FooterBanner.discount}</p>
-                <img src="https://i.ibb.co/BBwWDHV/headphones-a-4.webp" alt="" className='footer-banner-image'/>
-             
-
-            </div>
-            <div className="right">
-                <p>{FooterBanner.midText}</p>
-                <h3>{FooterBanner.smallText}</h3>
-                <p>{FooterBanner.desc}</p>
-                <Link href={`/product/${FooterBanner.product}`} className='footer-button'>
-                  <button className='footer-button'>{FooterBanner.buttonText}</button>
-                </Link>
-                
-            </div>
+      <div className="banner-desc">
+        <div className="">
+          <p>{FooterBanner.discount}</p>
+          <Image
+            src="https://i.ibb.co/BBwWDHV/headphones-a-4.webp"
+            alt={FooterBanner.product || "Footer Banner Image"}
+            className="footer-banner-image"
+            width={300} // adjust width
+            height={300} // adjust height
+          />
         </div>
+
+        <div className="right">
+          <p>{FooterBanner.midText}</p>
+          <h3>{FooterBanner.smallText}</h3>
+          <p>{FooterBanner.desc}</p>
+          <Link href={`/product/${FooterBanner.product}`} passHref>
+            <button className='footer-button'>{FooterBanner.buttonText}</button>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default FooterBanner
+export default FooterBanner;
